9a8f17e5e6cdfbeba57e9e9dae72a1f2532e2489
Haider Ali <haideralimughal002@gmail.com>
2023-03-21 15:35:18 +0500

    updated

--- controllers/todos.ts
+++ controllers/todos.ts
@@ -1,8 +1,9 @@
-import { config } from "https://deno.land/x/dotenv/mod.ts";
+// deno-lint-ignore-file
+import { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
 const { DATA_API_KEY, APP_ID } = config();
 
 const BASE_URI = `https://data.mongodb-api.com/app/${APP_ID}/endpoint/data/beta/action`;
-const DATA_SOURCE = "Cluster0";
+const DATA_SOURCE = "deno";
 const DATABASE = "todo_db";
 const COLLECTION = "todos";
 
@@ -14,3 +15,117 @@ const options = {
   },
   body: ""
 };
+
+
+const addTodo = async ({
+    request,
+    response,
+  }: {
+    request: any;
+    response: any;
+  }) => {
+    try {
+      if (!request.hasBody) {
+        response.status = 400;
+        response.body = {
+          success: false,
+          msg: "No Data",
+        };
+      } else {
+        const body = await request.body();
+        const todo = await body.value;
+        const URI = `${BASE_URI}/insertOne`;
+        const query = {
+          collection: COLLECTION,
+          database: DATABASE,
+          dataSource: DATA_SOURCE,
+          document: todo
+        };
+        options.body = JSON.stringify(query);
+        const dataResponse = await fetch(URI, options);
+        const { insertedId } = await dataResponse.json();
+        
+        response.status = 201;
+        response.body = {
+          success: true,
+          data: todo,
+          insertedId
+        };
+      }
+    } catch (err) {
+      response.body = {
+        success: false,
+        msg: err.toString(),
+      };
+    }
+  };
+  
+
+const getTodos = async ({ response }: { response: any }) => {
+    try {
+      const URI = `${BASE_URI}/find`;
+      const query = {
+        collection: COLLECTION,
+        database: DATABASE,
+        dataSource: DATA_SOURCE
+      };
+      options.body = JSON.stringify(query);
+      const dataResponse = await fetch(URI, options);
+      const allTodos = await dataResponse.json();
+  
+      if (allTodos) {
+        response.status = 200;
+        response.body = {
+          success: true,
+          data: allTodos,
+        };
+      } else {
+        response.status = 500;
+        response.body = {
+          success: false,
+          msg: "Internal Server Error",
+        };
+      }
+    } catch (err) {
+      response.body = {
+        success: false,
+        msg: err.toString(),
+      };
+    }
+  };
+
+
+  const getTodo = async ({
+    params,
+    response,
+  }: {
+    params: { id: string };
+    response: any;
+  }) => {
+    const URI = `${BASE_URI}/findOne`;
+    const query = {
+      collection: COLLECTION,
+      database: DATABASE,
+      dataSource: DATA_SOURCE,
+      filter: { todoId: parseInt(params.id) }
+    };
+    options.body = JSON.stringify(query);
+    const dataResponse = await fetch(URI, options);
+    const todo = await dataResponse.json();
+    
+    if (todo) {
+      response.status = 200;
+      response.body = {
+        success: true,
+        data: todo,
+      };
+    } else {
+      response.status = 404;
+      response.body = {
+        success: false,
+        msg: "No todo found",
+      };
+    }
+  };
+
+  export { addTodo, getTodos, getTodo }
--- routes.ts
+++ routes.ts
@@ -1,4 +1,16 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-const router = new Router(); // Create router
+import { 
+  addTodo, 
+  getTodos, 
+  getTodo 
+} from "./controllers/todos.ts"; // Import controller methods
+
+const router = new Router();
+
+// Implement routes
+router
+  .post("/api/todos", addTodo) // Add a todo
+  .get("/api/todos", getTodos) // Get all todos
+  .get("/api/todos/:id", getTodo); // Get one todo
 
 export default router;
